31d84a7645b89e5ee72a04a717c5448dc7f346ce
spirit-tv <tlitaudon@gmail.com>
2019-10-25 14:16:05 +0200

    Updates

---
+++ precache-manifest.13e75aa0788c49d2bc631bbb436798f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ded8f3aa015a9e5477e1f51dc534ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0aa38f4cdd526794c6",
+    "url": "/static/css/main.1dd1a8d3.chunk.css"
+  },
+  {
+    "revision": "344356e0f7486e4f49e4",
+    "url": "/static/js/2.d154df1f.chunk.js"
+  },
+  {
+    "revision": "cc0aa38f4cdd526794c6",
+    "url": "/static/js/main.79e8745d.chunk.js"
+  },
+  {
+    "revision": "4a86156a35133dddb70d",
+    "url": "/static/js/runtime-main.99ded50c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
